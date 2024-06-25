45a98b64e228778889a564a14168afbf92561223
Sebastian <sebastiandiaz1222@gmail.com>
2024-06-25 12:03:12 -0700

    added mini.move

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -9,13 +9,13 @@ return {
 		vim.keymap.set("n", "<a-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 		-- use alt to switch
-		vim.keymap.set("n", "<a-j>", function() harpoon:list():select(1) end)
-		vim.keymap.set("n", "<a-k>", function() harpoon:list():select(2) end)
-		vim.keymap.set("n", "<a-l>", function() harpoon:list():select(3) end)
-		vim.keymap.set("n", "<a-;>", function() harpoon:list():select(4) end)
+		-- vim.keymap.set("n", "<C-a-j>", function() harpoon:list():select(1) end)
+		-- vim.keymap.set("n", "<C-a-k>", function() harpoon:list():select(2) end)
+		-- vim.keymap.set("n", "<C-a-l>", function() harpoon:list():select(3) end)
+		-- vim.keymap.set("n", "<C-a-;>", function() harpoon:list():select(4) end)
 
 		-- Toggle previous & next buffers stored within Harpoon list
-		vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-		vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+		vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
+		vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
 	end
 }
--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,7 +1,28 @@
 return {
 	'echasnovski/mini.nvim',
 	version = false,
-	config = function ()
+	config = function()
 		require('mini.surround').setup({})
+		require('mini.move').setup({
+			-- Module mappings. Use `''` (empty string) to disable one.
+			mappings = {
+				-- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+				left = '<M-h>',
+				right = '<M-l>',
+				down = '<M-j>',
+				up = '<M-k>',
+
+				-- Move current line in Normal mode
+				line_left = '<M-h>',
+				line_right = '<M-l>',
+				line_down = '<M-j>',
+				line_up = '<M-k>',
+			}
+		})
+		-- Add surrounding with sa (in visual mode or on motion).
+		-- Delete surrounding with sd.
+		-- Replace surrounding with sr.
+		-- Find surrounding with sf or sF (move cursor right or left).
+		-- Highlight surrounding with sh.
 	end
 }
